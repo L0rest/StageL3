3da44e083b113c0f5d928909919cf548ea34fa6b
L0rest <jawad.maache@gmail.com>
2024-03-11 20:13:56 +0100

    Small patch

--- thermo_v3.3.py
+++ thermo_v3.3.py
@@ -828,18 +828,20 @@ def lire_fichier():
         file = open(fichier)
         texte = file.read()
         file.close()
-        file = open(fichier)
-        nb_ligne = len(file.readlines())
-        file.close()
-        texte = texte.replace(" ", " ; ")
     except (FileNotFoundError, TypeError):
         messagebox.showerror("Fichier", "Le fichier demandé n'existe pas!")
         return
 
     texte = texte.split("\n")
+    texte = [ligne for ligne in texte if ligne != '']
+    texte = [ligne for ligne in texte if 'flag' not in ligne]
+    nb_ligne = len(texte)
+
+    texte = [ligne.replace(" ",";") for ligne in texte]
     if any(";" not in ligne for ligne in texte) or any(";" in ligne for ligne in texte if ligne.count(";") != 4):
         messagebox.showerror("Fichier", "Le fichier demandé n'est pas bien formaté!")
         return
+
     # Ouvrir une fenêtre pour afficher les données du fichier
     fenetreFichier = tkt.Toplevel()
     fenetreFichier.title("Fichier")
@@ -860,7 +862,7 @@ def lire_fichier():
         row=0, column=4, sticky=NSEW)
 
     for i in range(nb_ligne):
-        ligne = texte[i].split(" ; ")
+        ligne = texte[i].split(";")
         for j in range(5):
             tkt.Label(fenetreFichier, text=ligne[j], font=("Helvetica", 11), borderwidth=1, relief="solid").grid(
                 row=i + 1, column=j, sticky=NSEW)
